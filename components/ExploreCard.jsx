243a6452e9d09346c3e0d3a36976872290d7b812
JamieChen007 <107536976+JamieChen007@users.noreply.github.com>
2023-11-03 01:02:31 +1100

    format code

--- components/ExploreCard.jsx
+++ components/ExploreCard.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 
 import styles from '../styles';
 import { fadeIn } from '../utils/motion';
+
 const ExploreCard = ({ id, imgUrl, title, index, active, handleClick }) => (
   <motion.div
     variants={fadeIn('right', 'spring', index * 0.5, 0.75)}
